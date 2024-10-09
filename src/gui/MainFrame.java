d5962e7de4fef37bd9d3af67306e1a222a32db65
David Gomez <id.gomez.prieto@gmail.com>
2024-10-09 12:25:07 +0200

    d

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -5,14 +5,16 @@ import java.awt.*;
 
 import gui.panels.MainPanels.BattlePanel;
 import gui.panels.MainPanels.PlayerSelectionPanel;
+import gui.panels.MainPanels.TitleScreen;
 
 public class MainFrame extends JFrame {
 
-    //Attributes - Panels
+    // Attributes - Panels
+    private final TitleScreen titleScreen = new TitleScreen(this);
     private final PlayerSelectionPanel playerSelectionPanel = new PlayerSelectionPanel();
     private BattlePanel battlePanel;
 
-    //Constructor
+    // Constructor
     public MainFrame() {
 
         this.setTitle("The Descent - RogueLike Game");
@@ -21,12 +23,19 @@ public class MainFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
 
-        this.add(playerSelectionPanel, BorderLayout.CENTER);
+        this.add(titleScreen, BorderLayout.CENTER);
 
         this.setVisible(true);
     }
 
-    //Methods
+    // Methods
+    public void changeToClassSelectionPanel() {
+        this.remove(titleScreen);
+        this.add(playerSelectionPanel, BorderLayout.CENTER);
+        this.repaint();
+        this.revalidate();
+    }
+
     public void changeToBattlePanel() {
         this.remove(playerSelectionPanel);
         battlePanel = new BattlePanel();
@@ -35,11 +44,9 @@ public class MainFrame extends JFrame {
         this.revalidate();
     }
 
-
-    //Setters and Getters
+    // Setters and Getters
     public BattlePanel getBattlePanel() {
         return battlePanel;
     }
 }
-
 
